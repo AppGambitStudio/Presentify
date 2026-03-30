d4691a0c58ea8c71f2e0756723234c20399791a2
Dhaval <dhaval.b.nagar@gmail.com>
2026-03-30 19:27:48 +0530

    feat: add component registry mapping ComponentType strings to React components

---
+++ src/components/slides/index.ts
@@ -0,0 +1,36 @@
+import { Heading } from "./Heading";
+import { Body } from "./Body";
+import { BulletList } from "./BulletList";
+import { NumberedSteps } from "./NumberedSteps";
+import { ComparisonTable } from "./ComparisonTable";
+import { TwoColumn } from "./TwoColumn";
+import { StatCallout } from "./StatCallout";
+import { QuoteBlock } from "./QuoteBlock";
+import { IconCard } from "./IconCard";
+import { CardGrid } from "./CardGrid";
+import { CodeBlock } from "./CodeBlock";
+import { ChartBlock } from "./ChartBlock";
+import { ImageBlock } from "./ImageBlock";
+import { TagList } from "./TagList";
+import { Spacer } from "./Spacer";
+import { Divider } from "./Divider";
+import { CTABox } from "./CTABox";
+import { ShowcaseCard } from "./ShowcaseCard";
+import { HeroIcon } from "./HeroIcon";
+import { PromptBlock } from "./PromptBlock";
+
+import type { ComponentType } from "@/lib/types";
+
+export const componentRegistry: Record<ComponentType, React.ComponentType<any>> = {
+  Heading, Body, BulletList, NumberedSteps, ComparisonTable,
+  TwoColumn, StatCallout, QuoteBlock, IconCard, CardGrid,
+  CodeBlock, ChartBlock, ImageBlock, TagList, Spacer,
+  Divider, CTABox, ShowcaseCard, HeroIcon, PromptBlock,
+};
+
+export {
+  Heading, Body, BulletList, NumberedSteps, ComparisonTable,
+  TwoColumn, StatCallout, QuoteBlock, IconCard, CardGrid,
+  CodeBlock, ChartBlock, ImageBlock, TagList, Spacer,
+  Divider, CTABox, ShowcaseCard, HeroIcon, PromptBlock,
+};
